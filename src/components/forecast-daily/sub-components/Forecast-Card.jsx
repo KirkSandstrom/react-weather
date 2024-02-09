359287f61339aaed2458dd04a1c78fa09e1de49c
Kirk Sandstrom <kirksandstrom@gmail.com>
2024-02-08 20:21:20 -0500

    fixed improper BEM naming

--- src/components/forecast-daily/sub-components/Forecast-Card.jsx
+++ src/components/forecast-daily/sub-components/Forecast-Card.jsx
@@ -11,43 +11,43 @@ export default function ForecastCard({
 }) {
   return (
     <div className="forecast-card">
-      <div className="forecast-card--row">
-        <p className="forecast-card--date">{date}</p>
+      <div className="forecast-card__row">
+        <p className="forecast-card__date">{date}</p>
         <img
-          className="forecast-card--condition-img"
+          className="forecast-card__condition-img"
           src={conditionImage}
         ></img>
-        <p className="forecast-card--temp-container">
-          <span className="forecast-card--max-temp">{maxTempF}&deg;</span>
-          <span className="forecast-card--min-temp"> /{mintempF}&deg;</span>
+        <p className="forecast-card__temp-container">
+          <span className="forecast-card__max-temp">{maxTempF}&deg;</span>
+          <span className="forecast-card__min-temp"> /{mintempF}&deg;</span>
         </p>
       </div>
-      <div className="forecast-card--row">
-        <p className="forecast-card--condition">{conditionDescription}</p>
+      <div className="forecast-card__row">
+        <p className="forecast-card__condition">{conditionDescription}</p>
       </div>
-      <div className="forecast-card--text-row">
-        <div className="forecast-card--text-row-group">
-          <div className="forecast-card--text-row-group-inner">
+      <div className="forecast-card__text-row">
+        <div className="forecast-card__text-row-group">
+          <div className="forecast-card__text-row-group-inner">
             <span>Chance of Rain</span>
             <span className="font-weight-bold">{dailyChanceOfRain}%</span>
           </div>
         </div>
-        <div className="forecast-card--text-row-group">
-          <div className="forecast-card--text-row-group-inner">
+        <div className="forecast-card__text-row-group">
+          <div className="forecast-card__text-row-group-inner">
             <span>Total Rain Fall</span>
             <span className="font-weight-bold">{totalPrecipIn}"</span>
           </div>
         </div>
       </div>
-      <div className="forecast-card--text-row">
-        <div className="forecast-card--text-row-group">
-          <div className="forecast-card--text-row-group-inner">
+      <div className="forecast-card__text-row">
+        <div className="forecast-card__text-row-group">
+          <div className="forecast-card__text-row-group-inner">
             <span>Chance of Snow</span>
             <span className="font-weight-bold">{dailyChanceOfSnow}%</span>
           </div>
         </div>
-        <div className="forecast-card--text-row-group">
-          <div className="forecast-card--text-row-group-inner">
+        <div className="forecast-card__text-row-group">
+          <div className="forecast-card__text-row-group-inner">
             <span>Total Snow Fall</span>
             <span className="font-weight-bold">{totalSnowIn}"</span>
           </div>
